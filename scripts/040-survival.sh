85d4040d0809906c1fe3e2530e457a5ce7d5d04a
akikuno <akikuno@users.noreply.github.com>
2021-02-09 19:01:19 +0900

    患者さんIDごとに遺伝子発現をCPM正規化しました

--- scripts/040-survival.sh
+++ scripts/040-survival.sh
@@ -2,17 +2,20 @@
 
 # 患者さんID, 性別, 生死, 観測期間, 遺伝子名, 遺伝子発現の6コラムのCSVファイルをつくります
 
-mkdir -p data/ICGC
+mkdir -p data/ICGC/
 
+# gzip -dc data/ICGC/donor.PAAD-US.tsv.gz| cut -f 1,5,6,17,18 | tr "\t" "@" | awk -F "@" '$3=="alive" {$4=$5; $4=""} $3=="deceased" {$4=$4}1'
+# gzip -dc data/ICGC/donor.PACA-CA.tsv.gz| cut -f 1,5,6,17,18 | awk 'NF==5' | head
+# gzip -dc data/ICGC/donor.PACA-AU.tsv.gz| cut -f 1,5,6,17,18 | awk 'NF==5' |head
+
+# gzip -dc data/ICGC/donor.* | grep -e DO32829 -e icgc_donor_id > tmp___
 gzip -dc data/ICGC/donor.* |
-  grep -v "icgc_donor_id" |
   cut -f 1,5,6,17,18 |
-  awk 'NF==5' |
-  sort > tmp_donor
+  tr "\t" "@" |
+  awk -F "@" '$3=="alive" {$4=$5; $4=""} $3=="deceased" {$4=$4}1' |
+  sort -t " " > tmp_donor
 
-gzip -dc data/ICGC/exp_seq.* |
-  grep -v "icgc_donor_id" |
-  awk -F "\t" 'BEGIN{OFS="\t"}{print $8,$1,$9}' |
+cat data/ICGC/exp_seq_* |
   sort |
   join -a 1 - data/HGNC/natmi_symbol.txt |
   join -a 1 - data/HGNC/natmi_ensemble.txt |
@@ -23,6 +26,6 @@ gzip -dc data/ICGC/exp_seq.* |
   join tmp_donor - |
   awk 'BEGIN {OFS=","; print "id", "sex", "status", "time", "gene", "exp"}
     {print $1,$2,$3,$4,$5,$6}' |
-  gzip -c > data/ICGC/survival.csv.gz
+  cat > data/ICGC/survival.csv
 
 rm tmp_donor
