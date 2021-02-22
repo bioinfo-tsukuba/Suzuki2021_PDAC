2dc92974c89d0a325c875afbd4d6851333eb5a12
akikuno <akikuno@users.noreply.github.com>
2021-02-22 16:57:35 +0900

    update: handle Ligand-recepter pair

--- scripts/survival-analysis/survival.R
+++ scripts/survival-analysis/survival.R
@@ -4,24 +4,31 @@ if (!require("pacman", quietly = TRUE)) install.packages("pacman")
 pacman::p_load(survival, survminer, tidyverse)
 
 df_raw <- read_csv("data/ICGC/survival.csv")
+df_LR <-
+  read_tsv("results/MD2/DiffEdges/upregulatedLR.tsv") %>%
+  select(`Cell-type pair`, `Ligand-receptor pair`)
 
 # Target gene lists
+genes <- c("SST->SSTR2","C1QTNF1->AVPR2")
 
-genes <- c("CD274","PDCD1","CXCL12","CXCR4")
-df_gene <- df_raw %>% filter(gene %in% genes)
+df_genes <-
+  map_dfr(genes, function(x) {
+    df_raw %>%
+    filter(gene %in% str_split(x, "->", simplify=TRUE)) %>%
+    mutate(LRpair = as.factor(x))}
+    )
 
 # high and low by median value
 
 df_plot <-
-  df_gene %>%
+  df_genes %>%
   mutate(status = if_else(status == "alive", 0, 1)) %>% # alive=0, dead=1
-  mutate(gene = as.factor(gene)) %>%
-  group_by(gene) %>%
+  group_by(LRpair) %>%
   mutate(exp_bin = if_else(exp > quantile(exp, 0.5), "high", "low")) %>%
   as.data.frame()
 
 fit <- survfit(Surv(time, status) ~ exp_bin, data = df_plot)
-ggsurvplot_facet(fit, df_plot, facet.by = "gene", pval = TRUE)
+ggsurvplot_facet(fit, df_plot, facet.by = "LRpair", pval = TRUE)
 
 ggsave("analysis/survival/testplot.png", dpi = 600)
 
