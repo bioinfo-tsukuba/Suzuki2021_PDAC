f48c41c6976a138d02e478c87cdee4b4bc4387bb
akikuno <akikuno@users.noreply.github.com>
2021-02-25 16:34:56 +0900

    working

--- scripts/survival-analysis/survival.R
+++ scripts/survival-analysis/survival.R
@@ -4,7 +4,8 @@ system("mkdir -p results/MD3/")
 if (!require("pacman", quietly = TRUE)) install.packages("pacman")
 pacman::p_load(survival, survminer, broom, tidyverse)
 
-df_raw <- read_csv("data/ICGC/survival.csv")
+df_survival <- read_csv("data/ICGC/survival.csv") %>%
+   mutate(status = if_else(status == "alive", 0, 1))  # alive=0, dead=1
 df_LR <-
   read_tsv("results/MD2/DiffEdges/tableForHeatmap.tsv")
 
@@ -13,21 +14,20 @@ df_LR_top <-
   group_by(category) %>%
   slice_max(delta_edge_specificity_weight, n = 20)
 
-# Target gene lists
-genes <- df_LR_top %>% pull(`ligandreceptor_pair`) %>% unique
-
-df_genes <-
-  map_dfr(genes, function(.x) {
-    df_raw %>%
-    filter(gene %in% str_split(.x, "->", simplify=TRUE)) %>%
-    mutate(LRpair = as.factor(.x))}
-    )
+df_nested <-
+  df_LR_top %>%
+  mutate(lr_pair = ligandreceptor_pair) %>%
+  separate(ligandreceptor_pair, c("ligand","receptor")) %>%
+  pivot_longer(-c(category, celltype_pair, lr_pair, delta_edge_specificity_weight),
+    names_to = "ligandreceptor", values_to = "gene") %>%
+  nest_join(df_survival)
 
 # high and low by median value
 
 df_plot <-
-  df_genes %>%
-  mutate(status = if_else(status == "alive", 0, 1)) %>% # alive=0, dead=1
+  df_nested %>%
+  rowwise() %>%
+
   group_by(LRpair) %>%
   mutate(exp_bin = if_else(exp > quantile(exp, 0.5), "high", "low")) %>%
   as.data.frame()
