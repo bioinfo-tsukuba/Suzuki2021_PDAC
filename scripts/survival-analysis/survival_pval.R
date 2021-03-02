d2028d7da0788413d8828990faad481ee5ce124f
akikuno <akikuno@users.noreply.github.com>
2021-03-02 17:53:14 +0900

    Compatible to delta weight specificity

--- scripts/survival-analysis/survival_pval.R
+++ scripts/survival-analysis/survival_pval.R
@@ -9,7 +9,9 @@ df_survival <-
   mutate(status = if_else(status == "alive", 0, 1)) # alive=0, dead=1
 
 df_LR <-
-  read_tsv("results/MD2/DiffEdges/tableForHeatmap.tsv")
+  read_tsv("results/MD2/DiffEdges/tableForHeatmap.tsv") %>%
+  filter(!str_detect(weight, "log2")) # remove log2-transformed data because of Inf value
+
 
 report_pval <- function(data) {
   survdiff(Surv(time, status) ~ exp_bin, data = data, rho = 1) %>%
@@ -22,10 +24,11 @@ df_pval <-
     df_LR %>%
     filter(category == .x) %>%
     # Extract top20
-    slice_max(delta_edge_specificity_weight, n = 20) %>%
+    group_by(weight) %>%
+    slice_max(value, n = 20) %>%
     mutate(lr_pair = ligandreceptor_pair) %>%
     separate(ligandreceptor_pair, c("ligand","receptor")) %>%
-    pivot_longer(-c(category, celltype_pair, lr_pair, delta_edge_specificity_weight),
+    pivot_longer(-c(category, celltype_pair, lr_pair, weight, value),
       names_to = "ligandreceptor", values_to = "gene") %>%
     # Bind LR data and survival data
     inner_join(df_survival, by = "gene") %>%
@@ -35,10 +38,10 @@ df_pval <-
     ungroup(id) %>%
     mutate(exp_bin = if_else(exp_sum > quantile(exp_sum, 0.5), "high", "low")) %>%
     # Report p-value
-    nest(data = !c(category, celltype_pair, lr_pair, delta_edge_specificity_weight)) %>%
+    nest(data = !c(category, celltype_pair, lr_pair, weight, value)) %>%
     mutate(pval = map_dbl(data, report_pval)) %>%
     filter(pval < 0.05) %>%
-    select(category, celltype_pair, lr_pair, delta_edge_specificity_weight, pval)
+    select(category, celltype_pair, weight, lr_pair, pval)
   }) %>%
   arrange(pval)
 
