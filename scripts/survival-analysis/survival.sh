395c1b3a61631a14d0a230bc06a1898e3f4ca094
akikuno <akikuno@users.noreply.github.com>
2021-03-19 08:42:06 +0900

    add survival analysis using all LR pairs

--- scripts/survival-analysis/survival.sh
+++ scripts/survival-analysis/survival.sh
@@ -15,17 +15,23 @@ if type curl >/dev/null 2>&1; then
 elif type wget >/dev/null 2>&1; then
   CMD_GET="wget --no-check-certificate -qO -"
 else
-  { echo 'No HTTP-GET/POST command found.'; exit 1; }
+  {
+    echo 'No HTTP-GET/POST command found.'
+    exit 1
+  }
 fi
 
 # --- gzip ------------------------------------------------
-type gzip >/dev/null 2>&1 || { echo 'gzip: command not found.'; exit 1; }
+type gzip >/dev/null 2>&1 || {
+  echo 'gzip: command not found.'
+  exit 1
+}
 
 #==============================================================================
 # Retrieve gene lists related to ligand-receptor interaction from NATMI
 #==============================================================================
 
-mkdir -p data/HGNC
+mkdir -p data/gene_info
 
 $CMD_GET https://asrhou.github.io/NATMI/ |
   awk 'BEGIN{RS=""} {$1=$1}1' |
@@ -34,45 +40,43 @@ $CMD_GET https://asrhou.github.io/NATMI/ |
   sed "s/ .*<td class=\"col4\">//" |
   cut -d " " -f 1,2 |
   tr " " "\n" |
-  sort -u > data/HGNC/natmi_genes.txt
+  sort -u >data/gene_info/natmi_genes.txt
 
 #==============================================================================
 # Retrieve HGNC gene list to convert Ensemble to Gene Symbol
 #==============================================================================
-
-mkdir -p data/HGNC
 
 $CMD_GET ftp://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/hgnc_complete_set.txt |
   sed 1d |
   cut -f 2,20 |
   awk 'NF==2' |
   sort |
-  join - data/HGNC/natmi_genes.txt |
+  join - data/gene_info/natmi_genes.txt |
   awk '{print $2,$1}' |
-  sort -t " " > data/HGNC/natmi_ensemble_symbol.txt
+  sort -t " " >data/gene_info/natmi_ensemble_symbol.txt
 
-cut -d " " -f 1 data/HGNC/natmi_ensemble_symbol.txt | sort | awk '{print $1,"LR"}' > data/HGNC/natmi_ensemble.txt
-cut -d " " -f 2 data/HGNC/natmi_ensemble_symbol.txt | sort | awk '{print $1,"LR"}' > data/HGNC/natmi_symbol.txt
+cut -d " " -f 1 data/gene_info/natmi_ensemble_symbol.txt | sort | awk '{print $1,"LR"}' >data/gene_info/natmi_ensemble.txt
+cut -d " " -f 2 data/gene_info/natmi_ensemble_symbol.txt | sort | awk '{print $1,"LR"}' >data/gene_info/natmi_symbol.txt
 
 #==============================================================================
 # Retrieve patients data from ICGC
 #==============================================================================
 
 mkdir -p data/ICGC
-echo "*" > data/ICGC/.gitignore
+echo "*" >data/ICGC/.gitignore
 
 for project in PAAD-US PACA-AU PACA-CA; do
-cat << FILE |
+  cat <<FILE |
 https://dcc.icgc.org/api/v1/download?fn=/current/Projects/$project/donor.$project.tsv.gz
 https://dcc.icgc.org/api/v1/download?fn=/current/Projects/$project/exp_seq.$project.tsv.gz
 https://dcc.icgc.org/api/v1/download?fn=/current/Projects/$project/specimen.$project.tsv.gz
 FILE
-  while read -r file; do
-    echo "$file"
-    [ -s "data/ICGC/${file##*/}" ] || $CMD_GET "$file" > "data/ICGC/${file##*/}"
-    # in case of failed first try...
-    [ -s "data/ICGC/${file##*/}" ] || $CMD_GET "$file" > "data/ICGC/${file##*/}"
-  done
+    while read -r file; do
+      echo "$file"
+      [ -s "data/ICGC/${file##*/}" ] || $CMD_GET "$file" >"data/ICGC/${file##*/}"
+      # in case of failed first try...
+      [ -s "data/ICGC/${file##*/}" ] || $CMD_GET "$file" >"data/ICGC/${file##*/}"
+    done
 done
 
 # CPM normalization by patiants ---------------------------
@@ -88,7 +92,7 @@ for project in PAAD-US PACA-AU PACA-CA; do
         cpm=value[nr]/sum[key]*1000000
         print gene[nr], key, cpm
       }
-    }' > data/ICGC/exp_seq_"$project".txt
+    }' >data/ICGC/exp_seq_"$project".txt
 done
 
 #==============================================================================
@@ -103,7 +107,7 @@ cat data/ICGC/nationwidechildrens.org_clinical_patient_paad.txt |
   sed "s/\[Not@Available\]/NA/" |
   sed "s/\[Discrepancy\]/Discrepancy/" |
   sort -t " " |
-  cat > tmp_tcga
+  cat >tmp_tcga
 
 gzip -dc data/ICGC/specimen.PAAD-US.tsv.gz |
   grep -v "icgc_donor_id" |
@@ -113,7 +117,7 @@ gzip -dc data/ICGC/specimen.PAAD-US.tsv.gz |
   join - tmp_tcga |
   # project, donor id, tumor, grade, stage
   awk '{print $2,$3,$6,$4,$5}' |
-  cat > tmp_tcga_patientinfo
+  cat >tmp_tcga_patientinfo
 
 gzip -dc data/ICGC/specimen.PACA-CA.tsv.gz data/ICGC/specimen.PACA-AU.tsv.gz |
   grep -v "icgc_donor_id" |
@@ -145,7 +149,7 @@ gzip -dc data/ICGC/specimen.PACA-CA.tsv.gz data/ICGC/specimen.PACA-AU.tsv.gz |
   # Annotate Primary or Metastasis
   awk '$NF ~ "M1" || $NF ~ "IV" {$(NF+1)="Metastasis"} {$(NF+1)="Primary"}1' |
   awk '{print $2,$1,$4,$6,$5}' |
-  cat > tmp_patients_info
+  cat >tmp_patients_info
 
 rm tmp_tcga tmp_tcga_patientinfo
 
@@ -155,7 +159,7 @@ rm tmp_tcga tmp_tcga_patientinfo
 
 mkdir -p data/ICGC/
 
-cut -d "," -f 1 tmp_patients_info | sort -u > tmp_patients_id
+cut -d "," -f 1 tmp_patients_info | sort -u >tmp_patients_id
 
 gzip -dc data/ICGC/donor.* |
   grep -v icgc_donor_id |
@@ -165,20 +169,20 @@ gzip -dc data/ICGC/donor.* |
   cut -d " " -f 1-4 |
   awk 'NF==4' |
   sort -t " " |
-  join - tmp_patients_id > tmp_donor
+  join - tmp_patients_id >tmp_donor
 
 cat data/ICGC/exp_seq_* |
   sort |
-  join -a 1 - data/HGNC/natmi_symbol.txt |
-  join -a 1 - data/HGNC/natmi_ensemble.txt |
+  join -a 1 - data/gene_info/natmi_symbol.txt |
+  join -a 1 - data/gene_info/natmi_ensemble.txt |
   awk '/LR$/ {print $1,$2,$3}' |
-  join -a 1 - data/HGNC/natmi_ensemble_symbol.txt |
+  join -a 1 - data/gene_info/natmi_ensemble_symbol.txt |
   awk 'NF==4 {$1=$4} {print $2,$1,$3}' |
   sort -t " " |
   join tmp_donor - |
   awk 'BEGIN {OFS=","; print "id", "sex", "status", "time", "gene", "exp"}
     {print $1,$2,$3,$4,$5,$6}' |
-  cat > data/ICGC/survival.csv
+  cat >data/ICGC/survival.csv
 
 rm tmp_patients_id tmp_donor
 
@@ -186,12 +190,12 @@ rm tmp_patients_id tmp_donor
 # Output patients info
 #==============================================================================
 
-cut -d, -f 1 data/ICGC/survival.csv | sort -u > tmp_donor_w_rnaseq
+cut -d, -f 1 data/ICGC/survival.csv | sort -u >tmp_donor_w_rnaseq
 
 sort -t " " tmp_patients_info |
   join - tmp_donor_w_rnaseq |
   awk 'BEGIN{OFS=","; print "ID", "Project", "Grade", "Primary_Metastasis", "Stage"}1' |
   tr " " "," |
-  cat > results/MD3/patients_info.csv
+  cat >results/MD3/patients_info.csv
 
 rm tmp_patients_info tmp_donor_w_rnaseq
---
+++ scripts/survival-analysis/survival_pval_all_LR.R
@@ -0,0 +1,55 @@
+# Report Peto & Peto modification of the Gehan-Wilcoxon test
+system("mkdir -p results/MD3/")
+
+if (!require("pacman", quietly = TRUE)) install.packages("pacman")
+pacman::p_load(survival, survminer, broom, dplyr, purrr, ggplot2, tidyr, rvest, janitor)
+
+df_survival <-
+  read_csv("data/ICGC/survival.csv") %>%
+  mutate(status = if_else(status == "alive", 0, 1)) # alive=0, dead=1
+
+df_all_LR <-
+  read_html("https://asrhou.github.io/NATMI/#2-connectomedb2020-") %>%
+  html_table() %>%
+  flatten_dfr() %>%
+  clean_names() %>%
+  mutate(pair = str_c(ligand_gene_symbol, receptor_gene_symbol, sep = "->")) %>%
+  select(pair, ligand_gene_symbol, receptor_gene_symbol) %>%
+  pivot_longer(-pair, names_to="LR", values_to="gene")
+
+report_pval <- function(data) {
+  survdiff(Surv(time, status) ~ exp_bin, data = data, rho = 1) %>%
+  glance() %>%
+  pull(p.value)
+}
+
+# df_test <- df_all_LR %>% head(100)
+
+start <- Sys.time()
+df_pval <-
+  df_survival %>%
+  inner_join(df_all_LR, key = "gene") %>%
+  group_by(pair, id, status, time) %>%
+  # high and low by median value
+  summarize(exp_sum = sum(exp)) %>%
+  ungroup(id, status, time) %>%
+  mutate(exp_bin = if_else(exp_sum > quantile(exp_sum, 0.5), "high", "low")) %>%
+  # median survival time
+  group_by(pair, exp_bin) %>%
+  mutate(median_time = quantile(time, 0.5)) %>%
+  # Report p-value
+  nest(data = !c(pair)) %>%
+  mutate(pval = map_dbl(data, report_pval)) %>%
+  filter(pval < 0.01) %>%
+  unnest(data) %>%
+  select(pair, pval, exp_bin, median_time) %>%
+  distinct() %>%
+  pivot_wider(c(pair, pval), names_from = "exp_bin", names_prefix = "median_day_", values_from = "median_time") %>%
+  mutate(diff_day = median_day_high - median_day_low) %>%
+  arrange(pval)
+
+end <- Sys.time() - start
+end # Time difference of 18.90914 secs
+
+write_csv(df_pval, "results/MD3/survival_all_LR.csv")
+
