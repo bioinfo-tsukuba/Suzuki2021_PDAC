d8e7177e3860ae43d58eaeecfa631be4d8461e4c
akikuno <akikuno@users.noreply.github.com>
2021-03-24 19:33:56 +0900

    generate umap fig

--- scripts/MD1/020-seurat.R
+++ scripts/MD1/020-seurat.R
@@ -1,116 +1,51 @@
-# This script makes NATMI input (emFile and annFile) for each sample (patient)
+###############################################################################
+# Setup
+###############################################################################
 
-# Definition of input and output paths
-path_10xdata <- "data/WeiLin_pdac10/For_Seurat/"
-path_cell_metadata <- "data/WeiLin_pdac10/PDAC10_meta.csv"
-path_outdir <- "processed_data/MD1"
+options(repos = "http://cran.us.r-project.org")
+if (!requireNamespace("pacman", quietly = TRUE)) install.packages("pacman")
+pacman::p_load(tidyverse, Seurat, umap, broom, glue)
 
-# Make output directory
-if(!dir.exists(path_outdir)){
-    dir.create(path_outdir, recursive=TRUE)
-}
+system("mkdir -p results/MD1")
 
-# Load library
-library(Seurat)
-library(tidyverse)
+###############################################################################
+# Generate Seurat object
+###############################################################################
 
-
-# Define P grouping
-P_low_grade <- c(
-    "P02",
-    "P05",
-    "P06",
-    "P08",
-    "P09",
-    "P10"
-)
-
-P_high_grade <- c(
-    "P01",
-    "P03",
-    "P04",
-    "P07"
-)
-
-list_P_grouping <-list(P_low_grade=P_low_grade, P_high_grade=P_high_grade)
-
-
-# Read data
-pdac_data <- Read10X(data.dir = path_10xdata)
-
-# Create a Seurat object
-pdac <- CreateSeuratObject(counts = pdac_data, project = "pdac")
-
-# Read cell metadata
-df_meta <- read_csv(path_cell_metadata, col_names=c("cell","id","type"))
-
-# Check input
+df_meta <- read_csv( "data/WeiLin_pdac10/PDAC10_meta.csv", col_names=c("cell","id","type"), skip = 1) %>%
+  mutate(grade = if_else(id %in% c("P02","P05","P06","P08","P09","P10"), "low", "high"))
 head(df_meta)
 
-# Add cell metadata to the Seurat object
-pdac@meta.data <- cbind(pdac@meta.data, df_meta[, c("id", "type")])
+data_seurat <- CreateSeuratObject(counts = Read10X(data.dir = "data/WeiLin_pdac10/For_Seurat/"), project = "pdac")
+data_seurat@meta.data <- cbind(data_seurat@meta.data, df_meta)
 
-# Yes, you can filter genes (features) here if you want
+###############################################################################
+# UMAP by each cell type
+###############################################################################
 
-# Define sample_id
-sample_ids <- unique(df_meta$id)
-
-
-pdac_umap <-
-  FindVariableFeatures(
-    object = pdac,
+data_umap <- map_dfr(unique(df_meta$type), function(.celltype) {
+  .data <- data_seurat[, str_which(df_meta$type, .celltype)]
+  .data_umap <-
+    FindVariableFeatures(
+    object = .data,
     selection.method = "vst",
     nfeatures = 2000,
     verbose = FALSE) %>%
   ScaleData(verbose = FALSE) %>%
   RunPCA(npcs = 30, verbose = FALSE) %>%
-  RunUMAP(reduction = "pca", dims = 1:30)
+  RunUMAP(reduction = "pca", dims = 1:30, verbose = FALSE)
 
-df_pdac_umap <-
-  pdac_umap[["umap"]]@"cell.embeddings" %>%
+  .data_umap[["umap"]]@"cell.embeddings" %>%
     as.data.frame() %>%
     rownames_to_column("cell") %>%
     as_tibble %>%
-    inner_join(df_meta, key = "cell") %>%
-    mutate(grade = if_else(id %in% P_low_grade, "low", "high"))
+    inner_join(df_meta, key = "cell")
+})
 
-ggplot(df_pdac_umap, aes(x = UMAP_1, y = UMAP_2)) +
+p_umap <- ggplot(data_umap, aes(x = UMAP_1, y = UMAP_2)) +
   geom_point(aes(color = grade)) +
-  facet_wrap(~type)
+  theme_bw() +
+  facet_wrap(~type, scale = "free")
+ggsave("results/MD1/umap_celltype.pdf", p_umap)
 
 
-# For each sampleid
-for(i in 1:length(list_P_grouping)){
-    id <- names(list_P_grouping)[i]
-    patients <- list_P_grouping[[i]]
-
-
-    # Get subset of cell names
-    df_meta %>%
-        filter(sampleid %in% patients) %>%
-        .$X1 -> cell_subset
-
-    # Get indicaters of cells from a specific sample (patient)
-    cells_subset_seurat <- WhichCells(object = pdac, cells = cell_subset)
-
-    # Extract gene expression data of cells from the specific sample (patient)
-    pdac_expr_subset <- GetAssayData(object = pdac, assay = "RNA", slot = "data")[, cells_subset_seurat]
-
-    # Convert subsetted assay data to matrix
-    pdac_expr_subset <- as(Class = 'matrix', object = pdac_expr_subset)
-
-    # Save expression matrixa as csv
-    path_output_emFile <- file.path(path_outdir, paste0(id, "_expression.csv"))
-    write.csv(x = pdac_expr_subset, file = path_output_emFile, quote = FALSE)
-
-    # Make metadata for cells from the specific sample (patient)
-    pdac_meta_subset <- pdac@meta.data[cells_subset_seurat, ] %>%
-        as_tibble(rownames="barcode") %>%
-        rename(annotation=types) %>%
-        select(barcode, annotation)
-
-    # Save metada as csv
-    path_output_annFile <- file.path(path_outdir, paste0(id, "_annot.csv"))
-    write_csv(pdac_meta_subset, path_output_annFile)
-
-}
