10a2e9498d7a0228be5b2eec8bce46200fa8a469
sayasuzu <ssayaka38@gmial.com>
2021-04-07 19:54:58 +0900

    NATMI LR pair survival analysis

--- scripts/MD3/NATMI_LRpairs_survival.sh
+++ scripts/MD3/NATMI_LRpairs_survival.sh
@@ -1,2 +1,21 @@
 #動作確認
 Rscript ./scripts/MD3/lrpair_pval.R
+
+
+#NATMI LR pair survival analysis
+
+#PAAD-US patients
+Rscript ./scripts/MD3/lrpair_pval.R \
+  data/ICGC/survival_PAAD-US.csv.gz \
+  data/NATMI_lrpair.csv > results/NATMI_lrpair_result.csv
+
+
+#PACA-AU patients
+Rscript ./scripts/MD3/lrpair_pval.R \
+  data/ICGC/survival_PACA-AU.csv.gz \
+  data/NATMI_lrpair.csv > results/NATMI_lrpair_result_AU.csv
+
+  #PACA-CA patients
+Rscript ./scripts/MD3/lrpair_pval.R \
+  data/ICGC/survival_PACA-CA.csv.gz \
+  data/NATMI_lrpair.csv > results/NATMI_lrpair_result_CA.csv
