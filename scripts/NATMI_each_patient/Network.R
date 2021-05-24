32417af0258e1826fd0a124ebe2ee9b84d31202f
yuifu <harukao.cb@gmail.com>
2021-05-24 23:39:30 +0900

    Update network analysis, adjusted enrichment

--- scripts/NATMI_each_patient/Network.R
+++ scripts/NATMI_each_patient/Network.R
@@ -1,5 +1,7 @@
 
 path_result <- "results/NATMI_each_patient/Dataframe_AtoE/Dataframe_E.csv"
+path_LR <- "results/Fig1/LR_adjPval_meanHR_screened.csv"
+path_LR_all <- "results/Fig1/LR_HR_adjPval.csv"
 path_outdir <- "results/NATMI_each_patient/Network"
 
 # Make output directory
@@ -12,50 +14,74 @@ library(tidyverse)
 library(qgraph)
 library(ggrepel)
 
+# Count the number of LR pairs
+dflr <- read_csv(path_LR)
+dflr %>% 
+    filter(meanHR<1) %>%
+    nrow -> n_HRL
+dflr %>% 
+    filter(meanHR>1) %>%
+    nrow -> n_HRH
+
+# Count the number of all LR pairs
+dflrall <- read_csv(path_LR_all)
+dflrall %>% 
+    nrow -> n_LR_all
+
 # Read data
 df1 <- read_csv(path_result, col_names=TRUE)
 
+# Calculate Adjusted enrichment
+df1 %>%
+    mutate(
+        adjusted_mean_NormHRL = mean_NormHRL/n_HRL*n_LR_all,
+        adjusted_mean_NormHRH = mean_NormHRH/n_HRH*n_LR_all
+    ) -> df1
+
+# Save data
+write_csv(df1, file.path(path_outdir, "adjusted_mean_enrichment_LRpairs.csv"))
+
 # Scatter plot
-min_value <- min(c(df1$mean_NormHRL, df1$mean_NormHRH)) 
-max_value <- max(c(df1$mean_NormHRL, df1$mean_NormHRH)) 
+min_value <- min(c(df1$adjusted_mean_NormHRL, df1$adjusted_mean_NormHRH)) 
+max_value <- max(c(df1$adjusted_mean_NormHRL, df1$adjusted_mean_NormHRH)) 
 
 df1 %>%
-    ggplot(aes(mean_NormHRL, mean_NormHRH, label=cell_type_pair)) +
-    geom_point(aes(color=mean_NormHRL/mean_NormHRH > 1.5)) + 
+    ggplot(aes(adjusted_mean_NormHRL, adjusted_mean_NormHRH, label=cell_type_pair)) +
+    geom_point(aes(color=adjusted_mean_NormHRL/adjusted_mean_NormHRH > 2)) + 
     geom_abline(intercept=0, slope=1) +
-    geom_abline(intercept=0, slope=1.5, linetype = "dashed") +
-    geom_abline(intercept=0, slope=1/1.5, linetype = "dashed") +
-    geom_text_repel(data=df1 %>% filter(mean_NormHRH/mean_NormHRL > 1.5 | mean_NormHRL/mean_NormHRH > 1.5)) +
+    geom_abline(intercept=0, slope=2, linetype = "dashed") +
+    geom_abline(intercept=0, slope=1/2, linetype = "dashed") +
+    geom_text_repel(data=df1 %>% filter(adjusted_mean_NormHRH/adjusted_mean_NormHRL > 2 | adjusted_mean_NormHRL/adjusted_mean_NormHRH > 2)) +
     theme(legend.position="bottom") + 
     lims(x=c(min_value, max_value), y=c(min_value, max_value)) +
-    labs(title="Table E, Mean of normalized average expression weight", 
+    labs(title="Adjusted mean enrichment of LR pairs", 
     x="LR pairs with HR<1", y="LR pairs with HR>1") -> g1
 g1
-ggsave(file.path(path_outdir, "scatter_mean_NormHRL_vs_mean_NormHRH.pdf"), g1)
+ggsave(file.path(path_outdir, "scatter_adjusted_mean_enrichment_HRL_vs_HRH.pdf"), g1)
 
 
 # Network
 ## Define edges
 df1 %>% 
     separate(cell_type_pair, sep="->", into=c("from", "to")) %>%
-    mutate(thickness = mean_NormHRL) %>%
+    mutate(thickness = adjusted_mean_NormHRL) %>%
     select(from, to, thickness) -> Edges_HRL
 
 df1 %>% 
     separate(cell_type_pair, sep="->", into=c("from", "to")) %>%
-    mutate(thickness = mean_NormHRH) %>%
+    mutate(thickness = adjusted_mean_NormHRH) %>%
     select(from, to, thickness) -> Edges_HRH
 
 df1 %>% 
     separate(cell_type_pair, sep="->", into=c("from", "to")) %>%
-    mutate(thickness = mean_NormHRL - mean_NormHRH) %>%
+    mutate(thickness = adjusted_mean_NormHRL - adjusted_mean_NormHRH) %>%
     select(from, to, thickness) -> Edges_diff
 
 # network_cell_type_HRL
 pdfname <- file.path(path_outdir, "network_cell_type_HRL.pdf")
 pdf(pdfname)
 qgraph(Edges_HRL, esize=5, 
-    theme = 'gray', layout="circle",
+    theme = 'gray', layout="circle", maximum=max_value,
     title = "LR pairs with HR<1")
 dev.off()
 
@@ -63,7 +89,7 @@ dev.off()
 pdfname <- file.path(path_outdir, "network_cell_type_HRH.pdf")
 pdf(pdfname)
 qgraph(Edges_HRH, esize=5, 
-    theme = 'gray', layout="circle",
+    theme = 'gray', layout="circle", maximum=max_value,
     title = "LR pairs with HR>1")
 dev.off()
 
---
+++ scripts/NATMI_each_patient/Network_subsetLR_grade.R
@@ -0,0 +1,137 @@
+
+path_result <- "/Users/ozakiharuka/Dropbox/research/2021/01_SSD/SSD/results/NATMI_each_patient/Dataframe_AtoE/Dataframe_D.csv"
+path_metadata <- "data/WeiLin_pdac10/Patient_data.csv"
+path_LR <- "results/Fig1/LR_adjPval_meanHR_screened.csv"
+path_LR_all <- "results/Fig1/LR_HR_adjPval.csv"
+path_outdir <- "results/NATMI_each_patient/Network_subsetLR_grade"
+
+# Make output directory
+if (!dir.exists(path_outdir)) {
+  dir.create(path_outdir, recursive = TRUE)
+}
+
+# Load library
+library(tidyverse)
+library(ggrepel)
+library(qgraph)
+
+# Read data
+df1 <- read_csv(path_result) %>%
+        replace_na(list(HRL=0, HRH=0))
+
+# Read metadata
+dfmeta <- read_csv(path_metadata) %>%
+    filter(Primary_or_Metasitasis=="Primary")
+
+# Count the number of LR pairs
+dflr <- read_csv(path_LR)
+dflr %>% 
+    filter(meanHR<1) %>%
+    nrow -> n_HRL
+dflr %>% 
+    filter(meanHR>1) %>%
+    nrow -> n_HRH
+
+# Count the number of all LR pairs
+dflrall <- read_csv(path_LR_all)
+dflrall %>% 
+    nrow -> n_LR_all
+
+
+#########################
+# Network for each grade 
+#########################
+## Define edges
+grades <- sort(unique(dfmeta$Grade))
+
+for(grade in grades){
+    dfmeta %>%
+    filter(Grade == grade) %>%
+    select(Patient_ID) %>%
+    { filter(df1, Patient %in% .$Patient_ID)} %>%
+    group_by(cell_type_pair) %>%
+    summarise(
+        mean_NormHRL = mean(NormHRL), 
+        mean_NormHRH = mean(NormHRH),
+        adjusted_mean_NormHRL = mean(NormHRL)/n_HRL*n_LR_all, 
+        adjusted_mean_NormHRH = mean(NormHRH)/n_HRH*n_LR_all, 
+    ) -> df1sub
+
+   # Save data
+    write_csv(df1, file.path(path_outdir, sprintf("adjusted_mean_enrichment_LRpairs_Grade%s.csv", grade)))
+
+    min_value <- min(c(df1sub$adjusted_mean_NormHRL, df1sub$adjusted_mean_NormHRH)) 
+    max_value <- max(c(df1sub$adjusted_mean_NormHRL, df1sub$adjusted_mean_NormHRH)) 
+
+    # Define edges, HR<1
+    df1sub %>%
+        separate(cell_type_pair, sep="->", into=c("from", "to")) %>%
+        mutate(thickness = adjusted_mean_NormHRL) %>%
+        select(from, to, thickness) -> Edges
+
+    pdfname <- file.path(
+        path_outdir, 
+        sprintf("network_cell_type_HRL_Grade%s.pdf", grade)
+    )
+    pdf(pdfname)
+    qgraph(Edges, esize=5, 
+        theme = 'gray', layout="circle", maximum=max_value,
+        title = sprintf("LR pairs with HR<1, Grade %s", grade))
+    dev.off()
+
+    # Define edges, HR>1
+    df1sub %>%
+        separate(cell_type_pair, sep="->", into=c("from", "to")) %>%
+        mutate(thickness = adjusted_mean_NormHRH) %>%
+        select(from, to, thickness) -> Edges
+
+    pdfname <- file.path(
+        path_outdir, 
+        sprintf("network_cell_type_HRH_Grade%s.pdf", grade)
+    )
+    pdf(pdfname)
+    qgraph(Edges, esize=5, 
+        theme = 'gray', layout="circle", maximum=max_value,
+        title = sprintf("LR pairs with HR>1, Grade %s", grade))
+    dev.off()
+
+    # Define edges, HR<1 - HR>1
+    df1sub %>%
+        separate(cell_type_pair, sep="->", into=c("from", "to")) %>%
+        mutate(thickness = adjusted_mean_NormHRL - adjusted_mean_NormHRH) %>%
+        select(from, to, thickness) -> Edges
+
+    pdfname <- file.path(
+        path_outdir, 
+        sprintf("network_cell_type_diff_Grade%s.pdf", grade)
+    )
+    pdf(pdfname)
+    qgraph(Edges, esize=5, 
+        theme = 'gray', layout="circle",
+        title = sprintf("Difference of HR<1 from HR>1, Grade %s", grade))
+    dev.off()
+
+
+
+    # Scatter plot
+
+    df1sub %>%
+        ggplot(aes(adjusted_mean_NormHRL, adjusted_mean_NormHRH, label=cell_type_pair)) +
+        geom_point(aes(color=adjusted_mean_NormHRL/adjusted_mean_NormHRH > 2)) + 
+        geom_abline(intercept=0, slope=1) +
+        geom_abline(intercept=0, slope=2, linetype = "dashed") +
+        geom_abline(intercept=0, slope=1/2, linetype = "dashed") +
+        geom_text_repel(
+            data=df1sub %>% filter(adjusted_mean_NormHRL/adjusted_mean_NormHRH > 2 | mean_NormHRL/mean_NormHRH > 2),
+            max.overlaps = Inf
+            ) +
+        theme(legend.position="bottom") + 
+        lims(x=c(min_value, max_value), y=c(min_value, max_value)) +
+        labs(title=sprintf("Adjusted mean enrichment of LR pairs, Grade%s", grade), 
+        x="LR pairs with HR<1", y="LR pairs with HR>1") -> g1
+    g1
+    ggsave(file.path(path_outdir, sprintf("scatter_adjuested_mean_enrichment_HRL_vs_HRH_Grade%s.pdf", grade)), g1)
+}
+
+# sessionInfo()
+sessionInfo()
