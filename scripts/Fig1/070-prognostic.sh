5db0d7f19127efb28f30e140a4af275f69063bbc
Akihiro Kuno <akikuno@users.noreply.github.com>
2021-06-11 20:29:52 +0900

    Update

--- scripts/Fig1/070-prognostic.sh
+++ scripts/Fig1/070-prognostic.sh
@@ -22,16 +22,18 @@ sed 1d results/Fig1/LR_adjPval_meanHR_screened.csv |
   join - tmp_unfavorable.txt |
   cat >results/Fig1/prognostic/unfavorable_genes.txt
 
-# joined favorable genes were really small... 予後が良いLR遺伝子が少ないからかな？
+rm tmp_
 
-cat data/NATMI_LR.csv |
-  awk '{sub("->","\n",$1); print $1}' |
-  sort -u |
-  join - tmp_favorable.txt |
-  wc -l
+# # joined favorable genes were really small... 予後が良いLR遺伝子が少ないからかな？
 
-cat data/NATMI_LR.csv |
-  awk '{sub("->","\n",$1); print $1}' |
-  sort -u |
-  join - tmp_unfavorable.txt |
-  wc -l
+# cat data/NATMI_LR.csv |
+#   awk '{sub("->","\n",$1); print $1}' |
+#   sort -u |
+#   join - tmp_favorable.txt |
+#   wc -l
+
+# cat data/NATMI_LR.csv |
+#   awk '{sub("->","\n",$1); print $1}' |
+#   sort -u |
+#   join - tmp_unfavorable.txt |
+#   wc -l
