552014ae78eca97e55f02dbd9f43e609e5cbfa09
Akihiro Kuno <akikuno@users.noreply.github.com>
2021-06-14 17:36:23 +0900

    Upload heatmap - Grade4において平均値が全LRペアの中央値よりも高いLRペアのみを対象としたヒートマップです. - 予後不良であり, スコアが2<3<4のものの図になります.

---
+++ scripts/Fig4/020-heatmap.R
@@ -0,0 +1,75 @@
+################################################################################
+# Initialization
+################################################################################
+
+if (!require("pacman", quietly = TRUE)) install.packages("pacman")
+pacman::p_load(tidyverse, janitor, patchwork)
+system("mkdir -p results/Fig4/")
+
+################################################################################
+# Input and format
+################################################################################
+
+df_lr <- read_csv("results/Fig4/median_threshold.csv", col_types = cols())
+
+files <- system("find results/NATMI_each_patient/ExtractEdges/ | grep Edges_lrc2p.csv | sort", intern = TRUE)
+patients_grade <-
+  read_csv("data/patients_info.csv", col_types = cols()) %>%
+  filter(Primary_Metastasis == "Primary") %>%
+  pull(Grade)
+df_natmi <- map2_dfr(files, patients_grade, ~ read_csv(.x, col_types = cols()) %>% mutate(grade = .y))
+
+df_natmi_formatted <-
+  df_natmi %>%
+  clean_names() %>%
+  select(grade, 1:4, edge_average_expression_weight) %>%
+  unite("CCI", c(sending_cluster, target_cluster), sep = "->") %>%
+  unite("LR", c(ligand_symbol, receptor_symbol), sep = "->") %>%
+  group_by(grade, CCI, LR) %>%
+  summarize(mean_weight = mean(edge_average_expression_weight)) %>%
+  ungroup()
+
+################################################################################
+# Filter LR:
+# (1) ENDOとDCの組み合わせを除きます.
+# (2) 2<3<4と発現が上昇しているLRペアを抽出します.
+################################################################################
+
+df_filtered <-
+  df_natmi_formatted %>%
+  filter(!CCI %in% c("Endo->Endo", "Endo->DC", "DC->Endo", "DC->DC")) %>%
+  group_by(CCI, LR) %>%
+  mutate(rank = rank(mean_weight) + 1) %>%
+  count(trend = (grade == rank)) %>%
+  filter(trend == TRUE, n == 3) %>%
+  select(CCI, LR)
+
+################################################################################
+# Join dataframes
+# (1) 予後に関わるLRペア群df_lrと結合します
+# (2) 前項のdf_filteredと結合します
+# (3) Grade4での平均の発現が中央値よりも高いペアを抽出します
+# (4) 予後不良因子のみを抽出します
+################################################################################
+
+df_plot <-
+  df_natmi_formatted %>%
+  inner_join(df_lr, by = c("CCI", "LR")) %>%
+  inner_join(df_filtered, by = c("CCI", "LR")) %>%
+  filter(candidate == TRUE) %>%
+  filter(prognosis == "poor")
+
+################################################################################
+# Plot
+################################################################################
+
+g_list <-
+  map(unique(df_plot$CCI), ~
+  df_plot %>%
+    filter(CCI == .x) %>%
+    ggplot(aes(x = grade, y = fct_rev(LR), fill = mean_weight)) +
+    geom_tile() +
+    labs(title = .x, x = "Grade", y = "LR pair", fill = "Score"))
+g <- wrap_plots(g_list)
+
+ggsave("results/Fig4/heatmap.pdf", g, width = 30, height = 30)
