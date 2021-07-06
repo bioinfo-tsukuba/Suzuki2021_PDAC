d4fff9247fdc2a7c52f4fc956d8ff143c4c8c4bd
Akihiro Kuno <akikuno@users.noreply.github.com>
2021-07-07 05:23:03 +0900

    update to output multiple pages in PDF

--- scripts/Fig1/042-plot-kaplan-meier-unfavorable.R
+++ scripts/Fig1/042-plot-kaplan-meier-unfavorable.R
@@ -2,9 +2,10 @@
 # Initialization
 ################################################################################
 options(warn = -1)
+options(repos="http://cran.us.r-project.org")
 
 if (!require("pacman", quietly = TRUE)) install.packages("pacman")
-pacman::p_load(survival, survminer, broom, tidyverse)
+pacman::p_load(survival, broom, tidyverse, gridExtra)
 
 ################################################################################
 # Input and format
@@ -32,17 +33,6 @@ df_lr <-
 # Main
 ################################################################################
 
-# report_pval <- function(data) {
-#   survdiff(Surv(time, status) ~ exp_bin, data = data, rho = 1) %>%
-#     glance() %>%
-#     pull(p.value)
-# }
-
-# report_hr <- function(data) {
-#   cox <- coxph(Surv(time, status) ~ exp_bin, data = data)
-#   1 / exp(cox$coefficients)
-# }
-
 df_exp_bin <-
   inner_join(df_survival, df_lr, by = "gene") %>%
   group_by(LR, cohort, id) %>%
@@ -53,25 +43,36 @@ df_exp_bin <-
   select(LR, cohort, time, status, exp_bin) %>%
   distinct()
 
-df_plot <- df_exp_bin %>%
+plot_func <- function(data, LR) {
+  data %>%
+    mutate(LR = LR) %>%
+    ggplot(aes(time, estimate, group = strata)) +
+    geom_line(aes(color = strata)) +
+    geom_ribbon(aes(ymin = conf.low, ymax = conf.high, fill = strata), alpha = .2) +
+    labs(x = "Time (days)", y = "Survival probability %") +
+    ylim(0, 1) +
+    scale_fill_discrete(labels = c("high", "low")) +
+    scale_color_discrete(labels = c("high", "low")) +
+    theme_bw() +
+    facet_grid(LR ~ cohort, scale = "free_x")
+}
+
+df_plot <-
+  df_exp_bin %>%
   group_nest(cohort, LR) %>%
   mutate(surv = map(data, ~ survfit(Surv(time, status) ~ exp_bin, data = .x))) %>%
   mutate(surv = map(surv, tidy)) %>%
   select(cohort, LR, surv) %>%
   unnest(surv) %>%
-  mutate(strata = str_remove_all(strata, "exp_bin="))
+  group_nest(LR) %>%
+  mutate(g = map2(data, LR, plot_func))
 
-g <-
-  ggplot(df_plot, aes(time, estimate, group = strata)) +
-  geom_line(aes(color = strata)) +
-  geom_ribbon(aes(ymin = conf.low, ymax = conf.high, fill = strata), alpha = .2) +
-  labs(x = "Time (days)", y = "Survival probability %") +
-  ylim(0, 1) +
-  theme_bw() +
-  facet_wrap(~ LR + cohort, ncol = 3, scale = "free_x")
+g <- marrangeGrob(df_plot$g, nrow = 7, ncol = 1)
 
 ################################################################################
 # Output
 ################################################################################
 
-ggsave("results/Fig1/kaplan_meier/unfavorable.pdf", g, dpi = 350, width = 10, height = 150, limitsize = FALSE)
+ggsave("results/Fig1/kaplan_meier/unfavorable.pdf", g,
+  dpi = 350, width = 10, height = 15, limitsize = FALSE
+)
